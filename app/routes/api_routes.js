94d8899b229c4c7ad1f1f795fa32406646db43fd
Timea Pusok <timea.pusok@sltom.ro>
2018-04-21 23:11:10 +0300

    added get and post for mbti test

--- app/routes/api_routes.js
+++ app/routes/api_routes.js
@@ -33,6 +33,20 @@ module.exports = function(app, db) {
 			}
 		});
 	});
+
+	app.get('/mbtiTest', (req, res) => {
+		const question = req.params.qestion;
+		const answerA = req.params.answerA;
+		const answerB = req.params.answerB;
+
+		db.collection('mbtiTest').find({}).toArray((err, item) => {
+			if (err) {
+				res.send({'error':'An error has occurred'});
+			} else {
+				res.send(item);
+			}
+		});
+	});
 	
 	app.post('/login', function(req, res) {
 		db.collection('userInfo').findOne({username: req.body.username },  function (err, user) {
@@ -64,6 +78,22 @@ module.exports = function(app, db) {
 		};
 		
 		db.collection('userInfo').insert(info, (err, result) => {
+			if (err) { 
+				res.send({ 'error': 'An error has occurred' }); 
+			} else {
+				res.send(result.ops[0]);
+			}
+		});
+	});
+
+	app.post('/mbtiTest', (req, res) => {
+		const info = { 
+			question: req.body.question,
+			answerA: req.body.answerA,
+			answerB: req.body.answerB
+		};
+		
+		db.collection('mbtiTest').insert(info, (err, result) => {
 			if (err) { 
 				res.send({ 'error': 'An error has occurred' }); 
 			} else {
