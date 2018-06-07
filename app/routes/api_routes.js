5d87c31f32e76b3e356f90dc3eb4559f381a4016
Timea Pusok <timea.pusok@altom.ro>
2018-06-07 19:12:50 +0300

    api_routes refactoring

--- app/routes/api_routes.js
+++ app/routes/api_routes.js
@@ -8,7 +8,8 @@ var jwt = require('jsonwebtoken');
 var config = require('../../dbConfig/config');
 
 module.exports = function(app, db) {
-	// LOGIN POST request
+
+		// --- LOGIN PAGE ---
 	app.post('/login', function(req, res) {
 		db.collection('userInfo').findOne({username: req.body.username },  function (err, user) {
 			if (err) {
@@ -24,10 +25,119 @@ module.exports = function(app, db) {
 			}
 		});
 	});
+		// --- PROFILE PAGE ---
+	app.get('/userInfo/:user', (req, res) => {
+		const user = req.params.user;
+		const details = { 'username': user };
 
-	// USERINFO COLLECTION
-	// ======================
-	//POST - add new user
+		db.collection('userInfo').findOne(details, (err, item) => {
+			if (err) {
+				res.send({'error':'An error has occurred'});
+			} else {
+				res.send(item);
+			}
+		});
+	});
+
+//to update user profiles
+	app.patch('/update/profile/:user', (req, res) => {
+		const user = req.params.user;
+		const details = { 'username': user };
+		const data = {
+			name: req.body.name,
+			email: req.body.email,
+			phone: req.body.phone,
+			persType: req.body.persType,
+			characteristics: req.body.characteristics
+		};
+
+		db.collection('userInfo').update(details, { $set: { profile : data  } }, (err, result) => {
+			if (err) {
+				res.send({'error':'An error has occurred'});
+			} else {
+				res.send({ success: true, data});
+			}
+		});
+	});
+
+	//used on profile page doGet('mbtiQuestions')
+	app.get('/mbtiQuestions', (req, res) => {
+		const question = req.params.question;
+		const answerA = req.params.answerA;
+		const answerB = req.params.answerB;
+		db.collection('mbtiTest').find({}).toArray((err, item) => {
+			if (err) {
+				res.send({'error':'An error has occurred'});
+			} else {
+				res.send(item);
+			}
+		});
+	});
+
+		// --- DASHBOARD PAGE ---
+	app.get('/userInfo/:user', (req, res) => {
+		const user = req.params.user;
+		const details = { 'username': user };
+
+		db.collection('userInfo').findOne(details, (err, item) => {
+			if (err) {
+				res.send({'error':'An error has occurred'});
+			} else {
+				res.send(item);
+			}
+		});
+	//used to display all teams for manager view
+	});
+	app.get('/allTeams', (req, res) => {
+		db.collection('userInfo').find({},{team:1}).toArray((err, item) => {
+			if (err) {
+				res.send({'error':'An error has occurred'});
+			} else {
+				res.send(item);
+			}
+		});
+	});
+
+	//used on dasboard to displayed the list of company members
+	app.get('/allUsers', (req, res) => {
+		// const name = req.params.name;
+		db.collection('userInfo').find({},{'profile.name':1, 'username':1}).toArray((err, item) => {
+			if (err) {
+				res.send({'error':'An error has occurred'});
+			} else {
+				res.send(item);
+			}
+		});
+	});
+//used on dasboard to populate tha bar chart for manager
+	app.get('/allPerstype', (req, res) => {
+		const perstype = req.params.perstype;
+		db.collection('userInfo').find({},{'profile.persType':1}).toArray((err, item) => {
+			if (err) {
+				res.send({'error':'An error has occurred'});
+			} else {
+				res.send(item);
+			}
+		});
+	});
+
+	// GET  Personality types from a given team for doughnut chart
+	app.get('/perstype/:team', (req, res) => {
+		const team = req.params.team;
+		const details = { 'team': team };
+		db.collection('userInfo').find(details,{'profile.name':1, 'profile.persType':1, 'username':1}).toArray((err, item) => {
+			if (err) {
+				res.send({'error':'An error has occurred'});
+			} else {
+				res.send(item);
+			}
+		});
+	});
+
+
+
+	// --- POPULATE DATA IN THE DATABASE ---
+	//used for adding users
 	app.post('/userInfo/create', (req, res) => {
 		const info = {
 			username: req.body.username,
@@ -50,162 +160,7 @@ module.exports = function(app, db) {
 			}
 		});
 	});
-
-	// GET user data
-	app.get('/userInfo/:user', (req, res) => {
-		const user = req.params.user;
-		const details = { 'username': user };
-
-		db.collection('userInfo').findOne(details, (err, item) => {
-			if (err) {
-				res.send({'error':'An error has occurred'});
-			} else {
-				res.send(item);
-			}
-		});
-	});
-
-	app.get('/userInfo', (req, res) => {
-		// const persType = req.params.persType;
-		// const team = req.params.team;
-		db.collection('userInfo').find({team: "A"},{persType:1}).toArray((err, item) => {
-			if (err) {
-				res.send({'error':'An error has occurred'});
-			} else {
-				res.send(item);
-			}
-		});
-	});
-
-	// GET ALL teams
-	app.get('/allTeams', (req, res) => {
-		db.collection('userInfo').find({},{team:1}).toArray((err, item) => {
-			if (err) {
-				res.send({'error':'An error has occurred'});
-			} else {
-				res.send(item);
-			}
-		});
-	});
-
-	// GET ALL users
-	app.get('/allUsers', (req, res) => {
-		// const name = req.params.name;
-		db.collection('userInfo').find({},{'profile.name':1, 'username':1}).toArray((err, item) => {
-			if (err) {
-				res.send({'error':'An error has occurred'});
-			} else {
-				res.send(item);
-			}
-		});
-	});
-
-	// GET ALL Personality types
-	app.get('/allPerstype', (req, res) => {
-		const perstype = req.params.perstype;
-		db.collection('userInfo').find({},{'profile.persType':1}).toArray((err, item) => {
-			if (err) {
-				res.send({'error':'An error has occurred'});
-			} else {
-				res.send(item);
-			}
-		});
-	});
-
-	// GET  Personality types from a given team
-	app.get('/perstype/:team', (req, res) => {
-		const team = req.params.team;
-		const details = { 'team': team };
-		db.collection('userInfo').find(details,{'profile.name':1, 'profile.persType':1, 'username':1}).toArray((err, item) => {
-			if (err) {
-				res.send({'error':'An error has occurred'});
-			} else {
-				res.send(item);
-			}
-		});
-	});
-
-	app.get('/userInfo/:user', (req, res) => {
-		const user = req.params.user;
-		const details = { 'username': user };
-
-		db.collection('userInfo').findOne(details, (err, item) => {
-			if (err) {
-				res.send({'error':'An error has occurred'});
-			} else {
-				res.send(item);
-			}
-		});
-	});
-
-
-	// DELETE request | Delete user with ID:
-	app.delete('/userInfo/:id', (req, res) => {
-		const id = req.params.id;
-		const details = { '_id': new ObjectID(id) };
-
-		db.collection('profileInfo').remove(details, (err, item) => {
-			if (err) {
-				res.send({'error':'An error has occurred'});
-			} else {
-				res.send('Note ' + id + ' deleted!');
-			}
-		});
-	});
-
-	// PATCH request | UPDATE user profile
-	app.patch('/update/profile/:user', (req, res) => {
-		const user = req.params.user;
-		const details = { 'username': user };
-		const data = {
-			name: req.body.name,
-			email: req.body.email,
-			phone: req.body.phone,
-			persType: req.body.persType,
-			characteristics: req.body.characteristics
-		};
-
-		db.collection('userInfo').update(details, { $set: { profile : data  } }, (err, result) => {
-			if (err) {
-				res.send({'error':'An error has occurred'});
-			} else {
-				res.send({ success: true, data});
-			}
-		});
-	});
-
-	// mbtiTest COLLECTION
-	// ======================
-	// GET MBTI test data
-	app.get('/mbtiQuestions', (req, res) => {
-		const question = req.params.question;
-		const answerA = req.params.answerA;
-		const answerB = req.params.answerB;
-		db.collection('mbtiTest').find({}).toArray((err, item) => {
-			if (err) {
-				res.send({'error':'An error has occurred'});
-			} else {
-				res.send(item);
-			}
-		});
-	});
-
-	// mbtiCharacteristics  COLLECTION
-	// ======================
-	// GET MBTI types and characteristics
-	app.get('/characteristics/:mbtiType', (req, res) => {
-		const mbtiType = req.params.mbtiType;
-		db.collection('mbtiCharacteristics').find({persType: new RegExp(mbtiType, 'i')}).toArray((err,item) => {
-			if (err) {
-				res.send({'error':'An error has occurred'});
-			} else {
-				res.send(item);
-			}
-		});
-	});
-
-	//db.collection('userInfo').findOne(details, (err, item) => {
-	// POST MBTI test data
+//used to add MBTI questions
 	app.post('/mbtiTest', (req, res) => {
 		const info = {
 			question: req.body.question,
@@ -222,6 +177,34 @@ module.exports = function(app, db) {
 		});
 	});
 
+// ----- WHERE ARE THESE USED?
+
+//I don't think it's used
+	app.get('/userInfo', (req, res) => {
+		// const persType = req.params.persType;
+		// const team = req.params.team;
+		db.collection('userInfo').find({team: "A"},{persType:1}).toArray((err, item) => {
+			if (err) {
+				res.send({'error':'An error has occurred'});
+			} else {
+				res.send(item);
+			}
+		});
+	});
+
+	//where is this used
+	app.get('/characteristics/:mbtiType', (req, res) => {
+		const mbtiType = req.params.mbtiType;
+		db.collection('mbtiCharacteristics').find({persType: new RegExp(mbtiType, 'i')}).toArray((err,item) => {
+			if (err) {
+				res.send({'error':'An error has occurred'});
+			} else {
+				res.send(item);
+			}
+		});
+	});
+
+//where is this used?
 	app.post('/mbtiCharacteristics', (req, res) => {
 		const info = {
 			persType: req.body.persType,
